4d1267fa8eaa41ff93eb1d7d4a49c70527382ead
Justin Vencel <failpunk@gmail.com>
2015-05-27 15:18:04 -0700

    Get grunt running

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+
+  // configure
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      client: [
+        'Gruntfile.js'
+      ]
+    },
+
+    browserify: {
+      options: {
+        transform: [ require('grunt-react').browserify ]
+      },
+      client: {
+        src: ['app/**/*.jsx'],
+        dest: 'web/js/app.js'
+      }
+    },
+
+    watch: {
+      react: {
+        files: 'app/**',
+        tasks: ['browserify']
+      }
+    }
+  });
+
+  // load
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // register
+  grunt.registerTask('default', ['jshint', 'browserify']);
+};
---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+/**
+ * @jsx React.DOM
+ */
+
+var MyComponent = React.createClass({
+
+  render: function () {
+    return (
+      <p>Howdy Dudesssss!</p>
+    );
+  }
+});
