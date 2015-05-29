1d800bde2199faca02aafe596542c024120323ba
Justin Vencel <failpunk@gmail.com>
2015-05-29 14:41:02 -0700

    Get basic form validation working

--- app/dashboard/settings/password/ChangePasswordForm.jsx
+++ app/dashboard/settings/password/ChangePasswordForm.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Router from 'react-router';
 import {Link, Navigation} from 'react-router';
+import Formsy from 'formsy-react';
+import {Input} from 'formsy-react-components';
+
+// Allows us to put this in validators object like all the other validators
+Formsy.addValidationRule('isRequired', function(values, value) {
+  return typeof value !== 'undefined' && value !== '' && value !== false;
+});
+
+// Custom validator to see if two fields match
+Formsy.addValidationRule('fieldsMatch', function(values, value, otherField) {
+  return values[otherField] == value;
+});
 
 let ChangePasswordForm = React.createClass({
 
@@ -10,26 +22,79 @@ let ChangePasswordForm = React.createClass({
     this.transitionTo('settings');
   },
 
+  enableButton: function () {
+    this.setState({
+      canSubmit: true
+    });
+  },
+
+  disableButton: function () {
+    this.setState({
+      canSubmit: false
+    });
+  },
+
+  submit() {
+    console.log('submit');
+  },
+
+  // Validation rules
+  validation: {
+    types: {
+      oldpassword: {
+        isRequired: true,
+        minLength: 6
+      },
+      password: {
+        isRequired: true,
+        minLength: 6
+      },
+      newpassword: {
+        isRequired: true,
+        minLength: 6,
+        fieldsMatch: 'password'
+      }
+    },
+    errors: {
+      isRequired:  'This is a required field',
+      minLength: 'Your password must be at least six characters',
+      fieldsMatch: 'You passwords do not match'
+    }
+  },
+
   render () {
     return (
-      <form action="/account/change-password" method="post" id="changePassword" noValidate="novalidate">
-        <div className="form-account">
-          <div className="form-group">
-            <label htmlFor="oldpassword" className="h4">Old Password</label>
-            <input type="password" id="oldpassword" className="txt form-control" placeholder="Old Password" autoComplete="off" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="h4">New Password</label>
-            <input type="password" id="password" className="txt form-control" placeholder="New Password" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="passwordConfirm" className="h4">Confirm New Password</label>
-            <input type="password" id="passwordConfirm" className="txt form-control" placeholder="Confirm New Password" />
-          </div>
-          <input type="submit" defaultValue="Update Password" className="btn btn-primary" />
-          <input type="button" defaultValue="Back" className="btn" onClick={this.backToSettings}/>
-        </div>
-      </form>
+    <Formsy.Form onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+      <Input
+        name="oldpassword"
+        type="password"
+        placeholder="Old Password"
+        label="Old Password"
+        autoComplete="off"
+        validations={this.validation.types.oldpassword}
+        validationErrors={this.validation.errors}/>
+
+      <Input
+        name="password"
+        type="password"
+        placeholder="New Password"
+        label="New Password"
+        autoComplete="off"
+        validations={this.validation.types.password}
+        validationErrors={this.validation.errors}/>
+
+      <Input
+        name="newpassword"
+        type="password"
+        placeholder="Confirm New Password"
+        label="Confirm New Password"
+        autoComplete="off"
+        validations={this.validation.types.newpassword}
+        validationErrors={this.validation.errors}/>
+
+      <input type="submit" defaultValue="Update Password" className="btn btn-primary" />
+      <input type="button" defaultValue="Back" className="btn" onClick={this.backToSettings}/>
+    </Formsy.Form>
     );
   }
 });
